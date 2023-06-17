04afd58b9a1c28113c639ce74a2aad3bd4dcd117
tvghjjhg <114023agf@gmail.com>
2023-06-17 16:12:31 +0800

    20230617

--- HW10/HW10.js
+++ HW10/HW10.js
@@ -1,42 +1,11 @@
-const { spawn } = require('child_process');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function executeCommand(cmd) {
-  const [command, ...args] = cmd.split(' ');
-  const childProcess = spawn(command, args, { shell: true });
-
-  childProcess.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-
-  childProcess.stderr.on('data', (data) => {
-    console.error(data.toString());
-  });
-
-  childProcess.on('close', (code) => {
-    console.log(`Child process exited with code ${code}`);
-    rl.prompt();
-  });
+async function eval(cmd) {
+    let args = cmd.split(' ')
+    let child = new Deno.Command(args[0], {args:args.slice(1)})
+    return await child.spawn().output()
 }
 
-rl.setPrompt('shell> ');
-rl.prompt();
-
-rl.on('line', (input) => {
-  const cmd = input.trim();
-
-  if (cmd === 'exit') {
-    rl.close();
-    return;
-  }
-
-  executeCommand(cmd);
-}).on('close', () => {
-  console.log('Exiting...');
-  process.exit(0);
-});
+while (true) {
+    let cmd = prompt("shell>")
+    if (cmd == 'exit') break
+    await eval(cmd)
+}
